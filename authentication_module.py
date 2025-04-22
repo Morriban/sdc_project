de84141387a91624ccc6121ea128a29be26d3615
Morriban <152575454+Morriban@users.noreply.github.com>
2025-04-22 01:18:27 -0400

    Added comments to authentication_module.py

--- authentication_module.py
+++ authentication_module.py
@@ -1,5 +1,8 @@
 import json
 
+
+# This function takes in a username and password, and will access the users.json file.
+# It opens the json file and compares it to
 def authenticate_user(username, password, users_file="users.json"):
     with open(users_file, "r") as f:
         users = json.load(f)
